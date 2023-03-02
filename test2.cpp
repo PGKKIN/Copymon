af44016a1b86790a4ce8d1d37db3fd71a992421e
12uminations <120168442+12uminations@users.noreply.github.com>
2023-03-02 15:09:24 +0700

    2/3/66

--- test2.cpp
+++ test2.cpp
@@ -1,3 +1,4 @@
+// Online C++ compiler to run C++ program online
 #include<iostream>
 #include<string>
 #include<ctime>
@@ -7,7 +8,6 @@
 using namespace std;
 
 class Unit{
-        string name;
         string type;
         int hp;
         int hpmax;
@@ -25,18 +25,102 @@ class Unit{
         string move4;//protect
         bool protect_on;
     public:
-        Unit(string,string,int,int,int,string,string,int,string,string,int,string,string,int,string,bool);
+        string name;
+        int spe;
+        Unit(string,string,int,int,int,int,string,string,int,string,string,int,string,string,int,string,bool);
 		vector<int> getSelect();
         vector<string> getSelect2();
         vector<string> getSelect3();
-
+        vector<int> getSelect4();
+        void choose(Unit *);
+		void showStatus();
+		void newTurn();
+		int attack(Unit &,int);
+		int beAttacked(int,string,int);
+		int heal();	
+		bool isDead();
 };
-Unit::Unit(string a,string b,int c,int d,int e,string f,string g,int h,string i,string j,int k,string l,string m,int n,string o,bool p){
+int Unit::beAttacked(int oppatk,string movetype,int moveatk){
+    double dmg;
+    if(protect_on) dmg = 0;
+    else{
+        if(movetype == "Fire"){
+            if(type == "Grass") dmg = (moveatk+oppatk-def)*2;
+            else if (type == "Ice") dmg = (moveatk+oppatk-def)*2;
+            else if (type == "Fire") dmg = (moveatk+oppatk-def)/2;
+            else if (type == "Water") dmg = (moveatk+oppatk-def)/2;
+        }
+        else if(movetype == "Grass"){
+            if(type == "Water") dmg = (moveatk+oppatk-def)*2;    
+            else if (type == "Ice") dmg = (moveatk+oppatk-def)/2;
+            else if (type == "Grass") dmg = (moveatk+oppatk-def)/2;
+            else if (type == "Fire") dmg = (moveatk+oppatk-def)/2;
+            else if (type == "Fairy") dmg = (moveatk+oppatk-def)*2;
+            else if (type == "Fighting") dmg = (moveatk+oppatk-def)*2;
+        }
+        else if(movetype == "Water"){
+            if(type == "Fire") dmg = (moveatk+oppatk-def)*2;    
+            else if (type == "Grass") dmg = (moveatk+oppatk-def)/2;
+            else if (type == "Water") dmg = (moveatk+oppatk-def)/2;
+        }
+        else if(movetype == "Fighting"){
+            if(type == "Normal") dmg = (moveatk+oppatk-def)*2;    
+            else if (type == "Ghost") dmg = 0;
+            else if (type == "Ice") dmg = (moveatk+oppatk-def)*2;
+            else if (type == "Dark") dmg = (moveatk+oppatk-def)*2;
+            else if (type == "Fighting") dmg = (moveatk+oppatk-def)/2;
+            else if (type == "Grass") dmg = (moveatk+oppatk-def)/2;
+            else if (type == "Fairy") dmg = (moveatk+oppatk-def)/2;
+        }
+        else if(movetype == "Dragon"){
+            if(type == "Dragon") dmg = (moveatk+oppatk-def)*2;    
+            else if (type == "Fairy") dmg = 0;
+        }
+        else if(movetype == "Ice"){
+            if(type == "Dragon") dmg = (moveatk+oppatk-def)*2;
+            else if (type == "Grass") dmg = (moveatk+oppatk-def)*2;
+            else if (type == "Water") dmg = (moveatk+oppatk-def)/2;
+            else if (type == "Fire") dmg = (moveatk+oppatk-def)/2;
+            else if (type == "Ice") dmg = (moveatk+oppatk-def)/2;
+        }
+        else if(movetype == "Dark"){
+            if(type == "Ghost") dmg = (moveatk+oppatk-def)*2;    
+            else if (type == "Fighting") dmg = (moveatk+oppatk-def)/2;
+        }
+        else if(movetype == "Ghost"){
+            if(type == "Ghost") dmg = (moveatk+oppatk-def)*2;    
+            else if (type == "Normal") dmg =0;
+            else if (type == "Dark") dmg = (moveatk+oppatk-def)/2;
+        }
+        else if(movetype == "Fairy"){
+            if(type == "Fighting") dmg = (moveatk+oppatk-def)*2;    
+            else if (type == "Dark") dmg = (moveatk+oppatk-def)*2;
+            else if (type == "Dragon") dmg = (moveatk+oppatk-def)*2;
+            else if (type == "Fire") dmg = (moveatk+oppatk-def)/2;
+            else if (type == "Grass") dmg = (moveatk+oppatk-def)/2;
+        }
+        else if(movetype == "Normal"){
+            if(type == "Ghost") dmg = 0;    
+        }
+    }
+    
+    hp -= dmg;
+	if(hp <= 0) hp = 0;
+	
+	return dmg;	
+}
+int Unit::attack(Unit &x,int y){
+    if(y==1) return x.beAttacked(atk,move1type,move1atk);
+    else if(y==2) return x.beAttacked(atk,move2type,move2atk);
+    else if(y==3) return x.beAttacked(atk,move3type,move3atk); 
+}
+Unit::Unit(string a,string b,int c,int d,int e,int q,string f,string g,int h,string i,string j,int k,string l,string m,int n,string o,bool p){
     name = a;
     type = b;
     hpmax = c;
     atk = d;
     def = e;
+    spe = q;
     move1 =f;
     move1type =g;
     move1atk =h;
@@ -61,6 +145,7 @@ vector<int> Unit::getSelect(){
      x.push_back(hpmax);
      x.push_back(atk);
      x.push_back(def);
+     x.push_back(spe);
      return x;
 }
 vector<string> Unit::getSelect3(){
@@ -74,37 +159,189 @@ vector<string> Unit::getSelect3(){
      s.push_back(move4);
      return s;
 }
+vector<int> Unit::getSelect4(){
+    vector<int> x;
+     x.push_back(move1atk);
+     x.push_back(move2atk);
+     x.push_back(move3atk);
+     return x;
+}
+int Unit::heal(){
+    int h=60;
+	if(hp + h > hpmax) h = hpmax - hp;
+	hp = hp + h;
+	return h;
+}	
+void ChoosePokemon(vector<Unit> &y,string x[],Unit a,Unit b,Unit c,Unit d,Unit e,Unit f,Unit g,Unit h,Unit i,Unit j){
+    cout<<"-----Choose your first pokemon----- " <<endl;
+    cout<<"Input Number to choose : ";
+    while (y.size() < 3) {
+        int pokemonIndex;
+        cin >> pokemonIndex;
 
-int main(){  
-    Unit poke1 ("Charizard","Fire",200,50,50,"Daimonji","Fire",75,"DragonClaw","Dragon",50,"BrickBreak","Fighting",50,"Protect",false);
-	Unit poke2 ("Blastoise","Water",200,25,75,"HydroPump","Water",75,"DarkPaluse","Dark",50,"IceBeam","Ice",50,"Protect",false);
-	Unit poke3 ("Venusaur","Grass",200,25,75,"SolarBeam","Grass",75,"SeedBomb","Grass",65,"Doube-Edge","Normal",60,"Protect",false);
-	Unit poke4 ("Snorlax","Normal",400,10,30,"HyperBeam","Normal",90,"Crunch","Dark",50,"HammerArm","Fighting",50,"Protect",false);
-	Unit poke5 ("Lucario","Fighting",175,75,50,"CloseCombat","Fighting",75,"DragonPaluse","Dragon",50,"ExtreamSpeed","Normal",60,"Protect",false);
-	Unit poke6 ("Gengar","Ghost",200,75,25,"DestinyBond","Ghost",75,"SukerPunch","Dark",50,"FirePunch","Fire",50,"Protect",false);
-	Unit poke7 ("Gardevoir","Fairy",200,75,25,"Moonblast","Fairy",75,"MysticalFire","Fire",50,"GrassKnot","Grass",50,"Protect",false);
-	Unit poke8 ("Garchomp","Dragon",200,50,50,"DragonRush","Dragon",75,"BrickBreak","Fighting",50,"FireFang","Fire",50,"Protect",false);
-	Unit poke9 ("Abomasnow","Ice",300,30,30,"Blizzard","Ice",75,"WoodHammer","Grass",60,"IcePunch","Ice",50,"Protect",false);
-	Unit poke10 ("Absol","Dark",200,60,40,"NightSlash","Dark",75,"Curse","Ghost",50,"RockSmash","Fighting",50,"Protect",false);
+    bool alreadyChosen = false;
+    for (int i = 0; i < y.size(); i++) {
+        if (y[i].name == x[pokemonIndex-1]) {
+            alreadyChosen = true;
+            break;
+        }
+    }
+    if (alreadyChosen) {
+        cout << "That Pokemon has already been selected. Please choose a different one." << endl ;
+        cout << "Please select your next Pokemon agian: " ;
+        continue;
+    }
+        switch (pokemonIndex) {
+        case 1:
+            y.push_back(a);
+            break;
+        case 2:
+            y.push_back(b);
+            break;
+        case 3:
+            y.push_back(c);
+            break;
+        case 4:
+            y.push_back(d);
+            break;
+        case 5:
+            y.push_back(e);
+            break;
+        case 6:
+            y.push_back(f);
+            break;
+        case 7:
+            y.push_back(g);
+            break;
+        case 8:
+            y.push_back(h);
+            break;
+        case 9:
+            y.push_back(i);
+            break;
+        case 10:
+            y.push_back(j);
+            break;
+        
+        default:
+            cout << "Invalid Pokemon selection. Please choose a valid option." << endl;
+            continue;
+    }
+    if (y.size() < 3) {
+        cout << "Please select your next Pokemon: " ;
+    } else {
+        break;
+    }
+    
+    }
+    cout << "-----You have selected the following Pokemon----- " << endl;
+        for (int i = 0; i < y.size(); i++) {
+    cout <<i+1 <<"."<< y[i].name<< endl;
+    }
+}
+void P1_Team(vector<Unit> &x){
+    cout<<"Your Current Pokemon : " <<"1." <<x[0].name <<endl;
+    cout<<"Your Other Pokemon : " ;
+    for(int i = 0 ; i < 3 ; i++){
+        if(x[i].name != x[0].name){
+            cout<<i+1<<"." <<x[i].name <<" ";
+        }
+   }
+   cout<<endl;
+}
 
-    vector<Unit> player1_pokemon = {poke1, poke2, poke3};
-    vector<Unit> allpoke = {poke1, poke2, poke3,poke4,poke5,poke6,poke7,poke8,poke9,poke10};
+void Switch(vector<Unit> &x){
+    cout<<"Switch The Current Pokemon : "  ;
+    int Switch_Number ;
+    cin >> Switch_Number ;
+    Unit temp = x[0] ;
+    x[0] = x[Switch_Number-1] ;
+    x[Switch_Number-1] = temp;
+}
+void showpk(vector<Unit> x){
     for(int i = 0 ; i < 10  ;i++){
         cout << i+1<<".";
         for(int j = 0 ; j < 2 ;j++){
-        	vector<string> stat = allpoke[i].getSelect2();
+        	vector<string> stat = x[i].getSelect2();
         	cout  << stat[j]<< " ";
         }
         cout<<" ";
         for(int k = 0 ; k < 1 ;k++){
-           vector<int> stat2 = allpoke[i].getSelect();
-            cout << "HP" <<stat2[k]<< " "<< "ATK" <<stat2[k+1]<< " "<< "DEF"<< stat2[k+2]<< " "; 
+           vector<int> stat2 = x[i].getSelect();
+            cout << "HP" <<stat2[k]<< " "<< "ATK" <<stat2[k+1]<< " "<< "DEF"<< stat2[k+2]<< " "<< "SPEED"<< stat2[k+3]<< " "; 
         }
 		for(int l = 0 ; l < 7 ;l+=2){
-        	vector<string> stat3 = allpoke[i].getSelect3();
+        	vector<string> stat3 = x[i].getSelect3();
         	cout  << stat3[l];
         	if(l<6) cout<<" Type:"<<stat3[l+1]<<" ";
         }
         cout<<endl;
     }
+}
+
+int main(){  
+    Unit poke1 ("Charizard","Fire",200,50,50,100,"Daimonji","Fire",75,"DragonClaw","Dragon",50,"BrickBreak","Fighting",50,"Protect",false);
+	Unit poke2 ("Blastoise","Water",200,25,75,78,"HydroPump","Water",75,"DarkPaluse","Dark",50,"IceBeam","Ice",50,"Protect",false);
+	Unit poke3 ("Venusaur","Grass",200,25,75,80,"SolarBeam","Grass",75,"SeedBomb","Grass",65,"Doube-Edge","Normal",60,"Protect",false);
+	Unit poke4 ("Snorlax","Normal",400,10,30,30,"HyperBeam","Normal",90,"Crunch","Dark",50,"HammerArm","Fighting",50,"Protect",false);
+	Unit poke5 ("Lucario","Fighting",175,75,50,90,"CloseCombat","Fighting",75,"DragonPaluse","Dragon",50,"ExtreamSpeed","Normal",60,"Protect",false);
+	Unit poke6 ("Gengar","Ghost",200,75,25,110,"DestinyBond","Ghost",75,"SukerPunch","Dark",50,"FirePunch","Fire",50,"Protect",false);
+	Unit poke7 ("Gardevoir","Fairy",200,75,25,85,"Moonblast","Fairy",75,"MysticalFire","Fire",50,"GrassKnot","Grass",50,"Protect",false);
+	Unit poke8 ("Garchomp","Dragon",200,50,50,102,"DragonRush","Dragon",75,"BrickBreak","Fighting",50,"FireFang","Fire",50,"Protect",false);
+	Unit poke9 ("Abomasnow","Ice",300,30,30,60,"Blizzard","Ice",75,"WoodHammer","Grass",60,"IcePunch","Ice",50,"Protect",false);
+	Unit poke10 ("Absol","Dark",200,60,40,75,"NightSlash","Dark",75,"Curse","Ghost",50,"RockSmash","Fighting",50,"Protect",false);
+
+    vector<Unit> player1_pokemon = {poke1, poke2, poke3};
+    vector<Unit> allpoke = {poke1, poke2, poke3,poke4,poke5,poke6,poke7,poke8,poke9,poke10};
+     string pokemonNames[] = {"Charizard","Blastoise","Venusaur","Snorlax","Lucario","Gengar","Gardevoir","Garchomp","Abomasnow","Absol",};
+    ////////////////////////////////////////////////////
+    vector<Unit> selected_pokemon1; //Arrey โปเกมอน;
+    vector<Unit> selected_pokemon2; //Arrey โปเกมอน
+    bool sd=true;
+    ////////////////////////////////////////////////////
+    if(sd){
+        showpk(allpoke);
+        cout<<"---------------------------------------\n";//เลือกโปร
+        cout<<"Player_1 Team Selection\n";
+        ChoosePokemon(selected_pokemon1,pokemonNames,poke1,poke2,poke3,poke4,poke5,poke6,poke7,poke8,poke9,poke10);
+        cout<<"---------------------------------------\n";//เลือกโปร
+        sd = false;
+    }
+    else {
+        showpk(allpoke);
+        cout<<"---------------------------------------\n";//เลือกโ
+        ChoosePokemon(selected_pokemon2,pokemonNames,poke1,poke2,poke3,poke4,poke5,poke6,poke7,poke8,poke9,poke10);
+        cout<<"---------------------------------------\n";//เลือกโปรแกมอน
+    }
+    while(true){
+		char player_action;
+		int num;
+		cout<<"['F' Fight] ['S' Switch] ['H' Heal] What will you do?  ";
+		cin>> player_action;
+		player_action = toupper(player_action);
+		if(player_action=='F'){
+		    int i = 0;
+			for(int l = 0 ; l < 7 ;l+=2){
+				vector<string> stat3 = selected_pokemon1[0].getSelect3();
+				cout <<i+1 <<"."<< stat3[l];
+				vector<int> stat4 = selected_pokemon1[0].getSelect4();
+				if(l<6) cout<<" MoveDamage:"<<stat4[i];
+				if(l<6) cout<<" Type:"<<stat3[l+1]<<"\n";
+				i++;
+            }
+            cout<<endl; 
+            cout<<"What move will your Copymon use? ";
+            cin>>num;
+            //if(num == 1) selected_pokemon1[0].();
+		}
+	    else if(player_action=='H') selected_pokemon1[0].heal();
+	    else if(player_action == 'S'){
+	        P1_Team(selected_pokemon1);
+            cout<<"---------------------------------------\n";
+            Switch(selected_pokemon1); 
+            cout<<"---------------------------------------\n";
+            P1_Team(selected_pokemon1);
+            cout<<"---------------------------------------\n";
+	    }
+	}
 }
