6404071a83e081d60152dbaeb5749e3b5e53f8bd
Natthawatnowan0837 <120445271+Natthawatnowan0837@users.noreply.github.com>
2023-03-07 17:34:08 +0700

    Update main.cpp

--- Pokemon/src/main.cpp
+++ Pokemon/src/main.cpp
@@ -1,138 +1,83 @@
-﻿#include <SDL.h>
-#include <SDL_ttf.h>
-#include <SDL_image.h>
-#include <string>
-#include <iostream>
-#include <cstdlib>
+#include "Button.h"
 
-using namespace std;
+int init = SDL_Init(SDL_INIT_EVERYTHING);
+int init2 = Mix_Init(0);
+SDL_Window* win = SDL_CreateWindow("PokemonGo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-const string WINDOW_TITLE = "Copymon";
+double delta = .0001;
+double time2 = SDL_GetTicks();
 
-SDL_Window* window = nullptr;
-SDL_Renderer* renderer = nullptr;
-TTF_Font* font = nullptr;
-SDL_Color font_color = { 255, 255, 255 };
-SDL_Rect start_button;
-SDL_Rect exit_button;
-SDL_Texture* background_texture = nullptr;
-
-void InitSDL() {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << endl;
-        exit(1);
+int main(int argc,char **argv) {
+    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024);
+    Mix_Music* music = Mix_LoadMUS("Audio/Battle.wav");
+    if (!music) {
+        cout << "Music Error" << Mix_GetError() << endl;
     }
-    window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (!window) {
-        cout << "Window could not be created! SDL_Error: " << SDL_GetError() << endl;
-        exit(1);
+    Mix_Chunk* sound = Mix_LoadWAV("Audio/Select.wav");
+    if (!sound) {
+        cout << "sound Error" << Mix_GetError() << endl;
     }
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer) {
-        cout << "Renderer could not be created! SDL_Error: " << SDL_GetError() << endl;
-        exit(1);
-    }
-    if (TTF_Init() < 0) {
-        cout << "SDL_ttf could not initialize! SDL_ttf Error: " << TTF_GetError() << endl;
-        exit(1);
-    }
-    font = TTF_OpenFont("Pokemon Solid.ttf", 24);
-    if (!font) {
-        cout << "Failed to load font! SDL_ttf Error: " << TTF_GetError() << endl;
-        exit(1);
-    }
-    if (IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG) {
-        cout << "SDL_image could not initialize! SDL_image Error: " << IMG_GetError() << endl;
-        exit(1);
-    }
-}
 
-void QuitSDL() {
-    TTF_CloseFont(font);
-    TTF_Quit();s
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_DestroyTexture(background_texture);
-    SDL_Quit();
-}
+    //Mix_PlayMusic(music, -1);
+    Mix_PlayChannel(-1, sound, 0);
 
-void LoadTextures() {
-    SDL_Surface* surface = IMG_Load("assent/Background.png");
-    if (!surface) {
-        cerr << "Failed to load background image! SDL_image Error: " << IMG_GetError() << endl;
-        exit(1);
-    }
-    background_texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
-}
 
-void DrawText(string text, int x, int y) {
-    SDL_Surface* surface = TTF_RenderText_Solid(font, text.c_str(), font_color);
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_Rect rect = { x, y, surface->w, surface->h };
-    SDL_FreeSurface(surface);
-    SDL_RenderCopy(renderer, texture, nullptr, &rect);
-    SDL_DestroyTexture(texture);
-}
-
-void DrawButton(SDL_Rect rect, string text) {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-    SDL_RenderFillRect(renderer, &rect);
-    DrawText(text, rect.x + 40, rect.y + 10);
-}
-
-bool CheckCollision(int x, int y, SDL_Rect rect) {
-    if (x > rect.x && x < rect.x + rect.w && y > rect.y && y < rect.y + rect.h) {
-        return true;
-    }
-    return false;
-}
-
-void HandleEvents(bool& running, bool& game_running) {
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-        switch (event.type) {
-        case SDL_QUIT:
-            running = false;
-            break;
-        case SDL_MOUSEBUTTONDOWN:
-            if (CheckCollision(event.button.x, event.button.y, start_button)) {
-                game_running = true;
-            }
-            else if (CheckCollision(event.button.x, event.button.y, exit_button)) {
-                running = false;
-            }
-            break;
-        }
-    }
-}
-int main(int argc, char* argv[]) {
-    InitSDL();
-    LoadTextures();
+    Mouse mouse;
+    
     bool running = true;
-    bool game_running = false;
 
-    start_button = { SCREEN_WIDTH / 2 - 75, SCREEN_HEIGHT / 2 - 50, 150, 50 };
-    exit_button = { SCREEN_WIDTH / 2 - 75, SCREEN_HEIGHT / 2 + 50, 150, 50 };
+    SDL_Rect rect{ 100,50,200,132 };
+    SDL_Rect rectBackground{ 0,1,1280,720 };
+
+    SDL_Texture* tex = IMG_LoadTexture(ren, "assent/Background.png");
+    if (!tex) {
+        cout << SDL_GetError() << endl;
+    }
+
+    Button FightButton;
+    FightButton.srect.y = 0 ;
+    FightButton.drect.x = SCREEN_WIDTH/1.2 - FightButton.drect.w/2;
+    FightButton.drect.y = 450;
+
 
     while (running) {
-        HandleEvents(running, game_running);
 
-        SDL_RenderCopy(renderer, background_texture, nullptr, nullptr); // render background texture first
+        mouse.update();
+        FightButton.update(mouse);
 
-        DrawButton(start_button, "Start");
-        DrawButton(exit_button, "Exit");
+        delta = (SDL_GetTicks() - time2) / 1000;
+        time2 = SDL_GetTicks();
 
-        SDL_RenderPresent(renderer);
-
-        if (game_running) {
-            // start your game here
-            game_running = false;
+        SDL_Event e;
+        
+        while (SDL_PollEvent(&e)) {
+            switch (e.type) {
+                case SDL_QUIT :
+                    running = false;
+                    break;
+                case SDL_MOUSEBUTTONUP:
+                    if (e.button.button == SDL_BUTTON_LEFT) {
+                        if (FightButton.isSelected) {
+                            cout << "Pokemon Fight\n";
+                            Mix_PlayChannel(-1, sound, 0);
+                        }
+                    }
+                }
+                
+                     
         }
-    }
 
-    QuitSDL();
-    return 0;
+        SDL_RenderCopy(ren, tex, NULL, &rectBackground);
+        FightButton.draws();
+        mouse.draw();
+        SDL_RenderPresent(ren);
+      
+
+    }
+    SDL_Quit();
+ 
+    return 0 ;
 }
